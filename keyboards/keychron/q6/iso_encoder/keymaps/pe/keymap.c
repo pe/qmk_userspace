173eb2d51e7bcd20cef9cc85f9ad3dcd99911af1
Philippe Eberli <p.eberli@gmx.ch>
2024-11-20 20:52:11 +0100

    Enable apple globe key

--- keyboards/keychron/q6/iso_encoder/keymaps/pe/keymap.c
+++ keyboards/keychron/q6/iso_encoder/keymaps/pe/keymap.c
@@ -25,6 +25,21 @@ enum layers{
   WIN_FN
 };
 
+//https://skip.house/blog/qmk-globe-key
+enum my_keycodes {
+ // apple globe key
+ AP_GLOB = SAFE_RANGE,
+};
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+  case AP_GLOB:
+    host_consumer_send(record->event.pressed ? AC_NEXT_KEYBOARD_LAYOUT_SELECT : 0);
+    return false;
+  }
+
+  return true;
+}
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_iso_110(
         KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     KC_MUTE,    G(S(KC_4)),  G(KC_SPC),  RGB_TOG,  KC_F16,   KC_MCTL,  G(KC_MCTL),  MC_LOCK,
@@ -32,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,                KC_DEL,      KC_END,     KC_PGDN,  KC_P7,    KC_P8,    KC_P9,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,    KC_ENT,                                        KC_P4,    KC_P5,    KC_P6,       KC_PPLS,
         KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,                 KC_UP,                KC_P1,    KC_P2,    KC_P3,
-        KC_LCTL,  KC_LOPT,  KC_LCMD,                                KC_SPC,                                 KC_RCMD,  KC_ROPT,  MO(MAC_FN), KC_RCTL,    KC_LEFT,     KC_DOWN,    KC_RGHT,  KC_P0,              KC_PDOT,     KC_PENT),
+        KC_LCTL,  KC_LOPT,  KC_LCMD,                                KC_SPC,                                 KC_RCMD,  KC_ROPT,  AP_GLOB,    KC_RCTL,    KC_LEFT,     KC_DOWN,    KC_RGHT,  KC_P0,              KC_PDOT,     KC_PENT),
     [MAC_FN] = LAYOUT_iso_110(
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_MPRV,  KC_MPLY,  KC_MNXT,  _______,  _______,  _______,    RGB_TOG,    _______,     _______,    _______,  _______,  _______,  _______,     _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,    _______,     _______,    _______,  _______,  _______,  _______,     _______,
